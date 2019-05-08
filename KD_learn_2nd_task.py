0cade777a04881dd86531f414f57c9664c50dcda
dxc33linger <48464022+dxc33linger@users.noreply.github.com>
2019-05-08 16:01:55 -0700

    Update KD_learn_2nd_task.py

--- KD_learn_2nd_task.py
+++ KD_learn_2nd_task.py
@@ -20,10 +20,7 @@ torch.backends.cudnn.deterministic = True
 np.random.seed(args.seed)  # Python random module.
 random.seed(args.seed)
 torch.manual_seed(args.seed)
-log_path = './log_KD_learn_2nd.txt'.format()
-
-# if os.path.exists(os.path.join('../results/',log_path)):
-# 	os.remove(os.path.join('../results/',log_path))
+log_path = './log.txt'.format()
 
 log_format = '%(asctime)s   %(message)s'
 logging.basicConfig(stream=sys.stdout, level=logging.INFO,
@@ -33,18 +30,9 @@ fh.setFormatter(logging.Formatter(log_format))
 logging.getLogger().addHandler(fh)
 logging.info("args = %s", args)
 
-logging.info("\n\n************************KD_learning_2nd_task.py Experiment starts here************************")
-logging.info("=============================== Get previous mask file ===================================")
-save_mask_file = '../mask_library/mask_{}_seed_{}.pickle'.format(args.join_mask_rate, args.seed)
-save_mask_fileR = '../mask_library/mask_{}_seed_{}_reverse.pickle'.format(args.join_mask_rate, args.seed)
-
-
 secondTask = continualNN.ContinualNN()
 
-
 task_list = secondTask.create_task()
-
-
 
 #
 logging.info("========================================== Get KD targets ==============================================")
@@ -88,20 +76,10 @@ for batch_idx, (data, target) in enumerate(partial_trainsetLoader):
 	print('batch {} Current task: {}'.format(batch_idx, target[0:10]))
 	break
 
-secondTask.load_mutant_specify_FC(0, 0)
-# secondTask.save_mutant(66, 1)
-logging.info('>>>>>>>>>>>>>>>>>>>>>>>>>> This training on previous T0 testing accu is : %.4f', secondTask.test(partial_testsetLoader_0))
-
 train_acc = np.zeros([1, args.num_epoch])
 test_acc = np.zeros([1, args.num_epoch])
 
-
 for epoch in range(0, args.num_epoch):
-	train_acc[0, epoch] = secondTask.train_with_mask_with_KD(epoch, partial_trainsetLoader_mix, save_mask_file, save_mask_fileR, KD_target_list, len_onehot= len(current_task)*2)
+	train_acc[0, epoch] = secondTask.train_with_mask_with_KD(epoch, partial_trainsetLoader_mix, KD_target_list, len_onehot= len(current_task)*2)
 	test_acc[0, epoch] = secondTask.test(partial_testsetLoader_mix)
-secondTask.save_mutant(77, 1)
-file_name = '../results/KD_learn_2nd_task_acc{0:.4f}_NBC0{1}_mask{2}_joinM{3}'.format(test_acc[0, -1], args.NB_C0, args.mask_rate, args.join_mask_rate)
-scio.savemat(file_name+'.mat', {'train_acc':train_acc, 'test_acc':test_acc})
-plot_curve(file_name, '../results', Multi_running=False)
-
 
